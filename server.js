f5cc2f86e4a6c60759c9baad0b88956b0c9c5388
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-07 13:56:33 -0700

    jpeg-js installed and successful image generation

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const app = require('./lib/app');
+//const app = require('./lib/app');
+const random = require('./lib/random-jpg');
 const pool = require('./lib/utils/pool');
 
 const PORT = process.env.PORT || 7890;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log(`Started on ${PORT}`);
+// });
+
+random.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
